97c9f085d6a77b233429c4629a8287cdcea31204
danny98m <dpmullen98@gmail.com>
2021-02-13 17:33:06 -0500

    Lint Footer component

--- src/components/UI/Footer/Footer.js
+++ src/components/UI/Footer/Footer.js
@@ -1,12 +1,27 @@
-import classes from "./Footer.module.css";
+import React from 'react';
+import classes from './Footer.module.css';
 
-const Footer = props => {
-  return (
-    <div className={classes.Footer}>
-      <p>Icons made by <a href="https://www.flaticon.com/authors/pixel-perfect" title="Pixel perfect">Pixel perfect</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></p>
-      <p>Weather data retrieved from <a href="https://openweathermap.org/">OpenWeather</a></p>
-    </div>
-  )
-}
+const Footer = () => (
+  <div className={classes.Footer}>
+    <p>
+      Icons made by
+      {' '}
+      <a href="https://www.flaticon.com/authors/pixel-perfect" title="Pixel perfect">Pixel perfect</a>
+      {' '}
+      from
+      {' '}
+      <a href="https://www.flaticon.com/" title="Flaticon">
+        www.flaticon.com
+      </a>
+    </p>
+    <p>
+      Weather data retrieved from
+      {' '}
+      <a href="https://openweathermap.org/">
+        OpenWeather
+      </a>
+    </p>
+  </div>
+);
 
 export default Footer;
