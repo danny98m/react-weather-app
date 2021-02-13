0b600f25dee074a551f7f008de41ffe9b156985a
danny98m <dpmullen98@gmail.com>
2021-02-13 16:28:35 -0500

    Lint App and Chart components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
-import { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import axios from 'axios';
+import { Switch, Route } from 'react-router-dom';
 import CurrentDay from './components/CurrentDay/CurrentDay';
 import WeatherCardList from './components/WeatherCardList/WeatherCardList';
 import DetailedView from './components/DetailedView/DetailedView';
 import Chart from './components/Chart/Chart';
-// default data set for optional use without api calls 
+// default data set for optional use without api calls
 import Data from './onecall.json';
-import axios from 'axios';
-import { Switch, Route } from 'react-router-dom';
 import { getLocaleInfo } from './utility/getLocaleInfo';
 
 function App() {
@@ -18,37 +18,40 @@ function App() {
   const [selected, setSelected] = useState();
 
   // scroll to the detail view when it is rendered
-  const executeScroll = () => detailRef.current.scrollIntoView() 
+  const executeScroll = () => detailRef.current.scrollIntoView();
 
   const getLocation = (lat, long, key) => {
     axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${long}&result_type=locality&key=${key}`)
-      .then(res => setlocationData(`${getLocaleInfo(res.data.results[0].address_components, 'locality', 'long')}, ${getLocaleInfo(res.data.results[0].address_components, 'administrative_area_level_1' ,'short')}, ${getLocaleInfo(res.data.results[0].address_components, 'country' ,'short')}`));
-  }
+      .then((res) => setlocationData(`${getLocaleInfo(res.data.results[0].address_components, 'locality', 'long')}, ${getLocaleInfo(res.data.results[0].address_components, 'administrative_area_level_1', 'short')}, ${getLocaleInfo(res.data.results[0].address_components, 'country', 'short')}`));
+  };
 
   const getWeather = (lat, long, key) => {
     axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&units=imperial&exclude=minutely&appid=${key}`)
-      .then(res => setWeatherData(res.data))
-      .catch(err => console.log(err))
-  }
+      .then((res) => setWeatherData(res.data))
+      .catch((err) => console.log(err));
+  };
 
   useEffect(() => {
-    // check if api keys setup (this will determine if the data used will be live or be dummy snapshot data)
+    // check if api keys setup
+    // this will determine if the data used will be live or be dummy snapshot data
     if (process.env.REACT_APP_WEATHER_API_KEY && process.env.REACT_APP_MAPS_API_KEY) {
       // if api keys are being used, ask for location permissions from user
-      navigator.geolocation.getCurrentPosition(res => {
+      navigator.geolocation.getCurrentPosition((res) => {
+        const lat = res.coords.latitude;
+        const long = res.coords.longitude;
         // If location permission granted: get weather from their location
-        getLocation(res.coords.latitude, res.coords.longitude, process.env.REACT_APP_MAPS_API_KEY);
-        getWeather(res.coords.latitude, res.coords.longitude, process.env.REACT_APP_WEATHER_API_KEY);
-      }, ()=> {
+        getLocation(lat, long, process.env.REACT_APP_MAPS_API_KEY);
+        getWeather(lat, long, process.env.REACT_APP_WEATHER_API_KEY);
+      }, () => {
         // If location permission denied: get weather from the api example (Texarkana, AR, US)
         getLocation(33.441792, -94.037689, process.env.REACT_APP_MAPS_API_KEY);
         getWeather(33.441792, -94.037689, process.env.REACT_APP_WEATHER_API_KEY);
-      })
+      });
     } else {
       // if api keys not setup, continue to use the snapshot dummy data
-      setlocationData('Texarkana, AR, US (snapshot data)')
+      setlocationData('Texarkana, AR, US (snapshot data)');
     }
-  }, [])
+  }, []);
 
   return (
     <div className="App">
@@ -57,28 +60,30 @@ function App() {
           <Chart hourlyTemps={weatherData.hourly} />
         </Route>
         <Route path="/">
-            <CurrentDay 
-              currentData={weatherData.current} 
-              hourlyArray={weatherData.hourly.slice(0, 25)} 
-              locationData={locationData} 
-              tz={weatherData.timezone}
-            />
-            <WeatherCardList 
-              weatherArray={weatherData.daily} 
-              selected={selected} 
-              setSelected={setSelected} 
-              tz={weatherData.timezone}
-            />
-            <div ref={detailRef}>
-              {selected >= 0 ? 
-                <DetailedView  
-                  executeScroll={executeScroll} 
-                  index={selected} 
+          <CurrentDay
+            currentData={weatherData.current}
+            hourlyArray={weatherData.hourly.slice(0, 25)}
+            locationData={locationData}
+            tz={weatherData.timezone}
+          />
+          <WeatherCardList
+            weatherArray={weatherData.daily}
+            selected={selected}
+            setSelected={setSelected}
+            tz={weatherData.timezone}
+          />
+          <div ref={detailRef}>
+            {selected >= 0
+              ? (
+                <DetailedView
+                  executeScroll={executeScroll}
+                  index={selected}
                   data={weatherData.daily[selected]}
                   tz={weatherData.timezone}
-                /> : null
-              }
-            </div>
+                />
+              )
+              : null}
+          </div>
         </Route>
       </Switch>
     </div>
--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,41 +1,57 @@
+import React from 'react';
+import {
+  ResponsiveContainer,
+  LineChart,
+  Line,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Label,
+} from 'recharts';
+import PropTypes from 'prop-types';
 import classes from './Chart.module.css';
 import { formatTimeString } from '../../utility/formatTime';
-import { ResponsiveContainer, LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Label} from 'recharts';
 
-const Chart = props => {
+const Chart = (props) => {
+  const { hourlyTemps } = props;
   const temps = [];
-  props.hourlyTemps.forEach(obj => {
-    temps.push({time: formatTimeString(obj.dt), temp: obj.temp})
-  })
+  hourlyTemps.forEach((obj) => {
+    temps.push({ time: formatTimeString(obj.dt), temp: obj.temp });
+  });
 
   function formatXAxis(tickItem) {
     // display just HH and AM/PM
     return (
       tickItem.length === 8
-      ? tickItem.slice(0, 2).concat(tickItem.slice(tickItem.length-2, tickItem.length))
-      : tickItem.slice(0, 1).concat(tickItem.slice(tickItem.length-2, tickItem.length))
-    )
+        ? tickItem.slice(0, 2).concat(tickItem.slice(tickItem.length - 2, tickItem.length))
+        : tickItem.slice(0, 1).concat(tickItem.slice(tickItem.length - 2, tickItem.length))
+    );
   }
 
   return (
     <div className={classes.Chart}>
       <h1>Change in Temperature Over the Next 48 Hours</h1>
       <ResponsiveContainer width="95%" height={500}>
-        <LineChart data={temps} margin={{top: 20, bottom: 20}}>
-          <Line type="monotone" dataKey="temp" stroke="#ef820d" activeDot={{r: 5}}/>
+        <LineChart data={temps} margin={{ top: 20, bottom: 20 }}>
+          <Line type="monotone" dataKey="temp" stroke="#ef820d" activeDot={{ r: 5 }} />
           <CartesianGrid stroke="#ccc" strokeDasharray="5 5" strokeOpacity="0.8" />
-          <XAxis dataKey="time" tickFormatter={formatXAxis} stroke="#fff" padding={{left: 10}}>
-            <Label value='Time (HR)' position='centerBottom' dy={20} style={{fill: '#fff'}} />
+          <XAxis dataKey="time" tickFormatter={formatXAxis} stroke="#fff" padding={{ left: 10 }}>
+            <Label value="Time (HR)" position="centerBottom" dy={20} style={{ fill: '#fff' }} />
           </XAxis>
           <YAxis stroke="#fff">
-            <Label angle={-90} value='Temp (F)' position='insideLeft' style={{textAnchor: 'middle', fill: '#fff'}} />
+            <Label angle={-90} value="Temp (F)" position="insideLeft" style={{ textAnchor: 'middle', fill: '#fff' }} />
           </YAxis>
-          <Tooltip/>
+          <Tooltip />
         </LineChart>
       </ResponsiveContainer>
       <p><small>For best results, view chart on desktop.</small></p>
     </div>
-  )
-}
+  );
+};
+
+Chart.propTypes = {
+  hourlyTemps: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default Chart;
