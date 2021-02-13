6834fe795c840d291856d82096ed27aa14b7a1af
danny98m <dpmullen98@gmail.com>
2021-02-13 17:56:16 -0500

    Lint WeatherCardList component

--- src/components/WeatherCardList/WeatherCardList.js
+++ src/components/WeatherCardList/WeatherCardList.js
@@ -1,33 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 import WeatherCard from './WeatherCard/WeatherCard';
 import classes from './WeatherCardList.module.css';
 
-const WeatherCardList = props => {
-  const changeSelected = index => {
+const WeatherCardList = ({
+  weatherArray,
+  selected,
+  setSelected,
+  tz,
+}) => {
+  const changeSelected = (index) => {
     // if the weathercard clicked was already in the active state, deactivate it
-    if (index === props.selected) {
-      props.setSelected(-1)
+    if (index === selected) {
+      setSelected(-1);
     } else {
       // set given weathercard to active state
-      props.setSelected(index);
+      setSelected(index);
     }
-  }
+  };
 
-  const weatherForecast = props.weatherArray.map((item, index) => (
-    <WeatherCard 
-      key={item.dt} 
-      data={item} 
-      changeSelected={changeSelected} 
-      index={index} 
-      selectedIndex={props.selected} 
-      tz={props.tz}
+  const weatherForecast = weatherArray.map((item, index) => (
+    <WeatherCard
+      key={item.dt}
+      data={item}
+      changeSelected={changeSelected}
+      index={index}
+      selectedIndex={selected}
+      tz={tz}
     />
-  ))
+  ));
 
   return (
     <div className={classes.WeatherCardList}>
       {weatherForecast}
     </div>
-  )
-}
+  );
+};
+
+WeatherCardList.propTypes = {
+  weatherArray: PropTypes.instanceOf(PropTypes.array).isRequired,
+  selected: PropTypes.number.isRequired,
+  setSelected: PropTypes.func.isRequired,
+  tz: PropTypes.string.isRequired,
+};
 
 export default WeatherCardList;
