f75d605c00f5d06c3e0a1fe1c5bb4b976a27d04f
danny98m <dpmullen98@gmail.com>
2021-02-13 17:52:06 -0500

    Lint WeatherCard component

--- src/components/WeatherCardList/WeatherCard/WeatherCard.js
+++ src/components/WeatherCardList/WeatherCard/WeatherCard.js
@@ -1,27 +1,51 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { formatDate } from '../../../utility/formatDate';
 import classes from './WeatherCard.module.css';
 
-const WeatherCard = props => {
-  // determine if card needs to be shown as 'selected' 
+const WeatherCard = ({
+  data,
+  changeSelected,
+  index,
+  selectedIndex,
+  tz,
+}) => {
+  // determine if card needs to be shown as 'selected'
   const mainClasses = [classes.WeatherCard];
-  if (props.index === props.selectedIndex) {
-    mainClasses.push(classes.SelectedBorder)
+  if (index === selectedIndex) {
+    mainClasses.push(classes.SelectedBorder);
   }
 
-  const weekdayShort = formatDate(props.data.dt, props.tz, 'weekdayShort');
+  const weekdayShort = formatDate(data.dt, tz, 'weekdayShort');
 
   return (
-    <div className={mainClasses.join(' ')} onClick={() => props.changeSelected(props.index)}>
+    <div role="button" tabIndex={index} className={mainClasses.join(' ')} onClick={() => changeSelected(index)}>
       <h2 className={classes.Day}>{weekdayShort}</h2>
-      <img src={`http://openweathermap.org/img/wn/${props.data.weather[0].icon}@2x.png`} alt=""/>
-      <p className={classes.Current}>{Math.round(props.data.temp.day)}&deg;</p>
+      <img src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`} alt="Weather Icon" />
+      <p className={classes.Current}>
+        {Math.round(data.temp.day)}
+        &deg;
+      </p>
       <div className={classes.MinMax}>
-        <p className={classes.Max}>{Math.round(props.data.temp.max)}&deg;</p>
-        <p className={classes.Min}>{Math.round(props.data.temp.min)}&deg;</p>
+        <p className={classes.Max}>
+          {Math.round(data.temp.max)}
+          &deg;
+        </p>
+        <p className={classes.Min}>
+          {Math.round(data.temp.min)}
+          &deg;
+        </p>
       </div>
     </div>
-  )
-}
+  );
+};
+
+WeatherCard.propTypes = {
+  data: PropTypes.instanceOf(PropTypes.object).isRequired,
+  changeSelected: PropTypes.func.isRequired,
+  index: PropTypes.number.isRequired,
+  selectedIndex: PropTypes.number.isRequired,
+  tz: PropTypes.string.isRequired,
+};
 
 export default WeatherCard;
