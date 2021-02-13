da6db8e682325222e5c75aa6423d030f76a4c544
danny98m <dpmullen98@gmail.com>
2021-02-13 16:40:24 -0500

    Lint CurrentDay component

--- src/components/CurrentDay/CurrentDay.js
+++ src/components/CurrentDay/CurrentDay.js
@@ -1,38 +1,64 @@
-import { Fragment } from 'react';
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
 
 import HourlyList from '../HourlyList/HourlyList';
 import classes from './CurrentDay.module.css';
 import { formatTimeString } from '../../utility/formatTime';
 import { formatDate } from '../../utility/formatDate';
 
-const CurrentDay = props => {
-  const timeString = formatTimeString(props.currentData.dt, props.tz);
-  const weekday = formatDate(props.currentData.dt, props.tz, 'weekdayLong')
+const CurrentDay = ({
+  currentData,
+  hourlyArray,
+  locationData,
+  tz,
+}) => {
+  const timeString = formatTimeString(currentData.dt, tz);
+  const weekday = formatDate(currentData.dt, tz, 'weekdayLong');
 
   return (
-    <Fragment>
+    <>
       <div className={classes.CurrentDay}>
-        <h1>{props.locationData}</h1>
-        <h2 className={classes.Day}>{weekday} {timeString}</h2>
-        <h2 className={classes.CurrentTemp}>{Math.round(props.currentData.temp)}&deg;</h2>
+        <h1>{locationData}</h1>
+        <h2 className={classes.Day}>
+          {weekday}
+          &nbsp;
+          {timeString}
+        </h2>
+        <h2 className={classes.CurrentTemp}>
+          {Math.round(currentData.temp)}
+          &deg;
+        </h2>
         <div className={classes.Grid}>
           <div>
             <h3>Feels</h3>
-            <h3>{Math.round(props.currentData.feels_like)}&deg;</h3>
+            <h3>
+              {Math.round(currentData.feels_like)}
+              &deg;
+            </h3>
           </div>
           <div className={classes.WeatherImage}>
-            <img src={`http://openweathermap.org/img/wn/${props.currentData.weather[0].icon}@4x.png`} alt={props.currentData.weather[0].description} />
-            <h3>{props.currentData.weather[0].description}</h3>
+            <img src={`http://openweathermap.org/img/wn/${currentData.weather[0].icon}@4x.png`} alt={currentData.weather[0].description} />
+            <h3>{currentData.weather[0].description}</h3>
           </div>
           <div>
             <h3>Humidity</h3>
-            <h3>{props.currentData.humidity}&#37;</h3>
+            <h3>
+              {currentData.humidity}
+              &#37;
+            </h3>
           </div>
         </div>
       </div>
-      <HourlyList hourlyArray={props.hourlyArray} tz={props.tz}/>
-    </Fragment>
-  )
-}
+      <HourlyList hourlyArray={hourlyArray} tz={tz} />
+    </>
+  );
+};
+
+CurrentDay.propTypes = {
+  currentData: PropTypes.instanceOf(PropTypes.object).isRequired,
+  hourlyArray: PropTypes.arrayOf(PropTypes.object).isRequired,
+  locationData: PropTypes.string.isRequired,
+  tz: PropTypes.string.isRequired,
+};
 
 export default CurrentDay;
